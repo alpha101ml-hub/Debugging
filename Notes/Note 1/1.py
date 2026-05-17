0bc82329c3e8ced4fd7a92be0611ee4db119af62
Kratik Jarwal <alpha101ml@gmail.com>
2026-05-17 22:24:49 +0530

    Add files via upload

---
+++ Notes/Note 1/1.py
@@ -0,0 +1,15 @@
+def sum_of_list(x: list):
+    sum = 0
+    
+    for i in x:
+        sum += i
+        
+    return sum
+
+imput_list = [10,20,30,40]
+print('summm')
+res_sum = sum_of_list(imput_list) # Add Breakpoint Here - the red dot before 9
+print('sasdasd')
+
+print(res_sum)
+
